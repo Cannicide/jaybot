df6f01dc502bbcedd37a7912bdd799b29dbaf4d5
Cannicide <41762964+Cannicide@users.noreply.github.com>
2020-01-19 13:57:56 -0500

    Made a small change to test arguments.

--- faq.js
+++ faq.js
@@ -2,6 +2,6 @@ var Command = require("./command");
 
 module.exports = new Command((message, args) => {
 
-    message.channel.send("Test this command (FAQ) with argument: " + args[0]);
+    message.channel.send("Test this command (FAQ) with arguments: " + args.join(", "));
 
 }, false);
