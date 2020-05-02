b547a923a7a0ed23cdfd558b2dc0f6b2b2b061ff
Cannicide <41762964+Cannicide@users.noreply.github.com>
2020-05-02 14:19:08 -0400

    Added description for help command.

--- help.js
+++ help.js
@@ -1,34 +1,44 @@
 var Command = require("./command");
-//var Interface = require("./interface")
+var Interface = require("./interface")
 
 module.exports = new Command("help", (message, args) => {
 
     //Rudimentary help command for now, will be improved once all commands are added
     var cmds = new Command().getCommands();
-    var res = "";
     var prefix = args[0];
+    var fields = [];
 
     cmds.forEach((item) => {
         if (!item.special) {
-            res += item.name + ": " + prefix + item.name;// + "\n";
+            var res = {
+                name: "",
+                value: ""
+            };
+            res.name = item.name.charAt(0).toUpperCase() + item.name.substring(1) + " Command";
+            res.value = (item.desc ? item.desc + "\n" : "") + "```fix\n" + prefix + item.name;// + "\n";
             let params = item.cmd.getArguments();
             if (!params) {
-                res += item.desc ? " - " + item.desc + "\n" : "\n";
+                res.value += "```\n** **";
             }
             else {
                 params.forEach((arg) => {
                     if ("optional" in arg && arg.optional == true) {
-                        res += ` [${arg.name}]`;
+                        res.value += ` [${arg.name}]`;
                     }
                     else {
-                        res += ` <${arg.name}>`;
+                        res.value += ` <${arg.name}>`;
                     }
                 });
-                res += item.desc ? " - " + item.desc + "\n" : "\n";
+                res.value += "```\n** **";
             }
+            fields.push(res);
         }
     });
 
-    message.channel.send(res);
+    let thumb = "https://cdn.discordapp.com/attachments/372124612647059476/431626525809573898/ZHFinal.png";
+    let embed = new Interface.Embed(message, thumb, fields);
+    embed.embed.title = "**Commands**";
 
-}, false);
+    message.channel.send(embed);
+
+}, false, false, "Gets a list of all commands, parameters, and their descriptions. Format: [optional] parameters, <required> parameters.");
