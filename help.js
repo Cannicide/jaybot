120e7f1edcc5bef61523fa5976a4d2924da29066
Cannicide <41762964+Cannicide@users.noreply.github.com>
2020-05-02 14:47:47 -0400

    Added credits to the help command.

--- help.js
+++ help.js
@@ -38,6 +38,7 @@ module.exports = new Command("help", (message, args) => {
     let thumb = "https://cdn.discordapp.com/attachments/372124612647059476/431626525809573898/ZHFinal.png";
     let embed = new Interface.Embed(message, thumb, fields);
     embed.embed.title = "**Commands**";
+    embed.embed.description = "ZhordeBot is the official Zombie Horde Discord Bot, created by Cannicide#2753 (JayCraft2) to provide informational resources to the community, make life easier for the staff, and offer a few additional sources of entertainment."
 
     message.channel.send(embed);
 
