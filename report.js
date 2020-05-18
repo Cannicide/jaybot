af883c9e8e97999c47a474d32a99fff0be8dec26
Cannicide <41762964+Cannicide@users.noreply.github.com>
2020-05-18 12:11:07 -0400

    No longer asks players "Bugs or Safespots" when in those particular channels.

--- report.js
+++ report.js
@@ -6,136 +6,150 @@ const plrepFormat = "https://zhorde.net/threads/how-to-report-a-player.875/";
 var Command = require("./command");
 var Interface = require("./interface");
 
-module.exports = new Command("report", (message, args) => {
+var reportTypes = ["Players", "Bugs", "Safespots"];
+var message;
 
-    var reportTypes = ["Players", "Bugs", "Safespots"];
+var reportFunction = (choice, menu) => {
+    if (!choice) {}
+    else {
+        var answer = choice.content;
+        var matchesType = false;
+
+        reportTypes.forEach((item) => {
+            if (item.toLowerCase() == answer.toLowerCase() || item.substring(0, item.length - 1).toLowerCase() == answer.toLowerCase()) {
+                matchesType = item;
+            }
+        });
+
+        if (matchesType) {
+            if (matchesType == "Bugs" || matchesType == "Safespots") {
+                //Report a bug/safespot
+                let thumb;
+                let bugThumb = "https://cdn.discordapp.com/attachments/668485643487412237/691701166408728676/bug.png";
+                let safespotThumb = "https://cdn.discordapp.com/attachments/668485643487412237/692087177047310356/safespot.png";
+
+                if (matchesType == "Bugs") thumb = bugThumb;
+                else thumb = safespotThumb;
+
+                let title = new Interface.Interface(message, "Give a concise and descriptive title for your bug/safespot report (one small sentence recommended):", (res1, init1) => {
+                    if (!res1){}
+                    else {
+                        var bugTitle = res1.content;
+                        init1.edit(`✅ Successfully entered a title for the bug report.`);
+
+                        let desc = new Interface.Interface(message, "Now give a detailed description of the bug/safespot you found, what map and arena you found it in (if applicable), and describe whether this is a safespot or a bug in general (1-6 sentences recommended).", (res2, init2) => {
+                            if (!res2){}
+                            else {
+                                var bugDesc = res2.content;
+                                init2.edit(`✅ Successfully entered a description for the bug report.`);
+
+                                let img = new Interface.Interface(message, "Attach **one** image or URL illustrating the bug that you found, or type `none` if you do not have an image/video/URL. **Image files** and **any URLs** are both accepted.", (res3, init3) => {
+                                    if (!res3){}
+                                    else {
+                                        var bugImage;
+                                        if (res3.content.toLowerCase() == "none") {
+                                            bugImage = thumb;
+                                        }
+                                        else if (res3.content.match("http")) {
+                                            bugImage = res3.content;
+                                        }
+                                        else {
+                                            var attachment = (res3.attachments).array();
+                                            bugImage = attachment[0].url;
+                                        }
+
+                                        init3.edit(`✅ Successfully set an image/file/URL, or a default image if one was not attached, for the bug report.`);
+
+                                        //Now post a bug report embed to the #bugs channel
+                                        let bugReport = new Interface.Embed(message, thumb, [
+                                            {
+                                                name: `Bug Report: **${bugTitle}**`,
+                                                value: bugDesc
+                                            },
+                                            {
+                                                name: `Bug Evidence`,
+                                                value: `[🔗](${bugImage})`
+                                            }
+                                        ]);
+
+                                        //Insert bugs to trello automagically
+                                        if (matchesType == "Bugs") {
+
+                                            var Trello = require('trello-node-api')(process.env.TRELLO_KEY, process.env.TRELLO_TOKEN);
+                                            var data = {
+                                                name: bugTitle,
+                                                desc: bugDesc + " [Added automatically by ZH-Bot]",
+                                                pos: 'bottom',
+                                                idList: process.env.BUGS_LIST, //REQUIRED
+                                                due: null,
+                                                dueComplete: false,
+                                                idMembers: [],
+                                                idLabels: [],
+                                                urlSource: bugImage
+                                            };
+
+                                            Trello.card.create(data).then(function (response) {
+                                                //console.log('Trello card creation response ', response);
+                                            }).catch(function (error) {
+                                                console.log('Trello card creation error:', error);
+                                            });
+                                        }
+
+                                        bugReport.embed["image"]["url"] = bugImage.match(/\.(jpeg|jpg|gif|png)$/) ? bugImage : "";
+                                        bugReport.embed.title = bugTitle;
+
+                                        if (matchesType == "Bugs") message.guild.channels.get(message.guild.channels.find("name", "bugs").id).send(bugReport);
+                                        else if (matchesType == "Safespots") message.guild.channels.get(message.guild.channels.find("name", "safespots").id).send(bugReport);
+                                        message.channel.send(`✅ Your bug report has been submitted, <@!${message.author.id}>`);
+                                    }
+                                })
+                            }
+                        })
+                    }
+                })
+            }
+            else if (matchesType == "Players") {
+                //Report a player
+                let thumb = "https://cdn.discordapp.com/attachments/372124612647059476/431626525809573898/ZHFinal.png";
+                let embed = new Interface.Embed(message, thumb, [
+                    {
+                        name: "Player Report Format",
+                        value: `To report a player, follow the [player report format](${plrepFormat}). Copy the template found in the post.`
+                    },
+                    {
+                        name: "Reporting the Player",
+                        value: `Create a new [player report thread](${plrepThread}) and paste the template. Provide detailed responses and solid evidence, following the format. Your report can only be seen by staff members, and they will review your report and take action as soon as possible.`
+                    }
+                ]);
+
+                message.channel.send(embed);
+            }
+        }
+        else {
+            message.channel.send("You did not select a valid object to report. Please try again.");
+        }
+
+        if (menu) menu.edit(`✅ Report-type selection successfully completed.`);
+    }
+};
+
+module.exports = new Command("report", (msg, args) => {
+
     var response = new Interface.FancyMessage("Report Issues", "Which of the following would you like to report?", reportTypes, {
         title: "#",
         bullet: "*"
     }).get();
 
-    var report = new Interface.Interface(message, response, (choice, menu) => {
-        if (!choice) {}
-        else {
-            var answer = choice.content;
-            var matchesType = false;
+    message = msg;
 
-            reportTypes.forEach((item) => {
-                if (item.toLowerCase() == answer.toLowerCase() || item.substring(0, item.length - 1).toLowerCase() == answer.toLowerCase()) {
-                    matchesType = item;
-                }
-            });
-
-            if (matchesType) {
-                if (matchesType == "Bugs" || matchesType == "Safespots") {
-                    //Report a bug/safespot
-                    let thumb;
-                    let bugThumb = "https://cdn.discordapp.com/attachments/668485643487412237/691701166408728676/bug.png";
-                    let safespotThumb = "https://cdn.discordapp.com/attachments/668485643487412237/692087177047310356/safespot.png";
-
-                    if (matchesType == "Bugs") thumb = bugThumb;
-                    else thumb = safespotThumb;
-
-                    let title = new Interface.Interface(message, "Give a concise and descriptive title for your bug/safespot report (one small sentence recommended):", (res1, init1) => {
-                        if (!res1){}
-                        else {
-                            var bugTitle = res1.content;
-                            init1.edit(`✅ Successfully entered a title for the bug report.`);
-
-                            let desc = new Interface.Interface(message, "Now give a detailed description of the bug/safespot you found, what map and arena you found it in (if applicable), and describe whether this is a safespot or a bug in general (1-6 sentences recommended).", (res2, init2) => {
-                                if (!res2){}
-                                else {
-                                    var bugDesc = res2.content;
-                                    init2.edit(`✅ Successfully entered a description for the bug report.`);
-
-                                    let img = new Interface.Interface(message, "Attach **one** image or URL illustrating the bug that you found, or type `none` if you do not have an image/video/URL. **Image files** and **any URLs** are both accepted.", (res3, init3) => {
-                                        if (!res3){}
-                                        else {
-                                            var bugImage;
-                                            if (res3.content.toLowerCase() == "none") {
-                                                bugImage = thumb;
-                                            }
-                                            else if (res3.content.match("http")) {
-                                                bugImage = res3.content;
-                                            }
-                                            else {
-                                                var attachment = (res3.attachments).array();
-                                                bugImage = attachment[0].url;
-                                            }
-
-                                            init3.edit(`✅ Successfully set an image/file/URL, or a default image if one was not attached, for the bug report.`);
-
-                                            //Now post a bug report embed to the #bugs channel
-                                            let bugReport = new Interface.Embed(message, thumb, [
-                                                {
-                                                    name: `Bug Report: **${bugTitle}**`,
-                                                    value: bugDesc
-                                                },
-                                                {
-                                                    name: `Bug Evidence`,
-                                                    value: `[🔗](${bugImage})`
-                                                }
-                                            ]);
-
-                                            //Insert bugs to trello automagically
-                                            if (matchesType == "Bugs") {
-
-                                                var Trello = require('trello-node-api')(process.env.TRELLO_KEY, process.env.TRELLO_TOKEN);
-                                                var data = {
-                                                    name: bugTitle,
-                                                    desc: bugDesc + " [Added automatically by ZH-Bot]",
-                                                    pos: 'bottom',
-                                                    idList: process.env.BUGS_LIST, //REQUIRED
-                                                    due: null,
-                                                    dueComplete: false,
-                                                    idMembers: [],
-                                                    idLabels: [],
-                                                    urlSource: bugImage
-                                                };
-
-                                                Trello.card.create(data).then(function (response) {
-                                                    //console.log('Trello card creation response ', response);
-                                                }).catch(function (error) {
-                                                    console.log('Trello card creation error:', error);
-                                                });
-                                            }
-
-                                            bugReport.embed["image"]["url"] = bugImage.match(/\.(jpeg|jpg|gif|png)$/) ? bugImage : "";
-                                            bugReport.embed.title = bugTitle;
-
-                                            if (matchesType == "Bugs") message.guild.channels.get(message.guild.channels.find("name", "bugs").id).send(bugReport);
-                                            else if (matchesType == "Safespots") message.guild.channels.get(message.guild.channels.find("name", "safespots").id).send(bugReport);
-                                            message.channel.send(`✅ Your bug report has been submitted, <@!${message.author.id}>`);
-                                        }
-                                    })
-                                }
-                            })
-                        }
-                    })
-                }
-                else if (matchesType == "Players") {
-                    //Report a player
-                    let thumb = "https://cdn.discordapp.com/attachments/372124612647059476/431626525809573898/ZHFinal.png";
-                    let embed = new Interface.Embed(message, thumb, [
-                        {
-                            name: "Player Report Format",
-                            value: `To report a player, follow the [player report format](${plrepFormat}). Copy the template found in the post.`
-                        },
-                        {
-                            name: "Reporting the Player",
-                            value: `Create a new [player report thread](${plrepThread}) and paste the template. Provide detailed responses and solid evidence, following the format. Your report can only be seen by staff members, and they will review your report and take action as soon as possible.`
-                        }
-                    ]);
-
-                    message.channel.send(embed);
-                }
-            }
-            else {
-                message.channel.send("You did not select a valid object to report. Please try again.");
-            }
-
-            menu.edit(`✅ Report-type selection successfully completed.`);
-        }
-    });
+    if (message.channel.name.toLowerCase().match("bug")) {
+        reportFunction({content: "Bugs"});
+    }
+    else if (message.channel.name.toLowerCase().match("safespot")) {
+        reportFunction({content: "Safespots"});
+    }
+    else {
+        var report = new Interface.Interface(msg, response, reportFunction);
+    }
 
 }, false, false, "Report bugs, safespots, and players.");
