2831fd2f5a06812446673000e2d7225a965da675
Cannicide <41762964+Cannicide@users.noreply.github.com>
2020-06-16 22:35:21 -0400

    Added statistics webpage, discord profile fetcher webpage, and dashboard webpage to website.js.

--- website.js
+++ website.js
@@ -1,8 +1,30 @@
-function setup(app) {
-
+function setup(app, disc) {
+    const bodyParser = require("body-parser");
+    app.use(bodyParser.urlencoded({ extended: true }));
+    
     app.get("/", (req, res) => {
         res.sendFile(__dirname + "/views/login.html");
     });
+
+    app.post("/dashboard", (req, res) => {
+        if (req.body.password == process.env.DASHBOARD_PASSWORD) {
+            res.sendFile(__dirname + "/views/index.html");
+        }
+        else {
+            res.send("Wrong password. Try again <a href='/'>here</a>.");
+        }
+    });
+  
+  app.get("/profile/:user/:discrim", (req, res) => {
+    res.header("Access-Control-Allow-Origin", "https://cannicideapi.glitch.me");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    var tag = req.params.user + "#" + req.params.discrim;
+    res.send(disc.users.find(m => m.tag == tag).displayAvatarURL);
+  });
+  
+  app.get("/statistics", (req, res) => {
+      res.sendFile(__dirname + "/storage/statistics.json");
+  });
 
 }
 
