18c6fcdc0cc47aa4c40d1662c504df4004d2f1f2
Cannicide <41762964+Cannicide@users.noreply.github.com>
2021-01-14 19:33:42 -0500

    Added new interpreter system.

--- interpreter.js
+++ interpreter.js
@@ -1,50 +1,58 @@
 //A non-command system to interpret messages that are not commands and auto-respond/auto-react if necessary
 
-var report = require("./commands/report");
 var evg = require("./evg");
-var polls = require("./commands/poll");
 
-function Interpreter(message) {
+function Interpreter() {
 
-    var Reactions = new evg("reactions");
+    var Reactions = evg.remodel("reactions");
+    const interpreters = {
+      message: [],
+      dm: [],
+      reaction: []
+    }
 
-    this.interpret = (args) => {
+    /**
+     * Interprets a guild message. Formerly named 'interpret()'.
+     */
+    this.message = (message, args) => {
 
-        //Suggestion reactions:
-        if (args[0].toLowerCase().match("suggestion:") && message.channel.name.toLowerCase().match("suggestions")) {
-            
-            message.react("713053971757006950");
+        //Message interpreter format:
+        /*
+          {
+            filter: function(message, args),
+            response: function(message, args)
+          }
+        */
 
-            //Send nay after yea
-            setTimeout(() => {
-                message.react("713053971211878452");
-            }, 100);
-            
-        }
-        //Bug Colon system:
-        else if (args[0].toLowerCase().match("bug:") && message.channel.name.toLowerCase().match("bug")) {
-            report.colon(message, args, "Bugs");
-        }
-        //Safespot Colon system:
-        else if (args[0].toLowerCase().match("safespot:") && message.channel.name.toLowerCase().match("safespot")) {
-            report.colon(message, args, "Safespots");
-        }
+        var intp = interpreters.message.find(elem => elem.filter(message, args));
+
+        if (intp) intp.response(message, args);
 
     }
 
-    this.interpretDM = (args, client) => {
+    /**
+     * Interprets a DM (direct message). Formerly named 'interpretDM()'.
+     */
+    this.dm = (message, args) => {
 
-        var DiscordSRZ = require("./discordsrz");
+        //DM interpreter format:
+        /*
+          {
+            filter: function(message, args),
+            response: function(message, args)
+          }
+        */
 
-        //DiscordSRZ Code Link:
-        if (args[0].length == 5 && args[0].match(/[0-9]{5}/g)) {
-            var srz = new DiscordSRZ(client);
-            new srz.Link(args[0], message);
-        }
+        var intp = interpreters.dm.find(elem => elem.filter(message, args));
+
+        if (intp) intp.response(message, args);
 
     }
 
-    this.interpretReaction = (reaction, user, isAdding) => {
+    /**
+     * Inteprets a reaction. Formerly named 'interpretReaction()'.
+     */
+    this.reaction = (reaction, user, isAdding) => {
 
         if (user.bot) return;
 
@@ -52,51 +60,91 @@ function Interpreter(message) {
         var emote = reaction.emoji.name;
         var emoteID = reaction.emoji.id;
 
-        var cache = Reactions.get();
-        var inCache = cache.find(entry => (entry.name == emote || entry.id == emoteID || (Array.isArray(entry.name) && entry.name.includes(emote)) || (Array.isArray(entry.id) && entry.id.includes(emoteID))) && entry.messageID == message.id);
+        var inCache = Reactions.find(entry => (entry.name == emote || entry.id == emoteID || (Array.isArray(entry.name) && entry.name.includes(emote)) || (Array.isArray(entry.id) && entry.id.includes(emoteID))) && entry.messageID == message.id);
 
         //The given message is not to be interpreted by the interpreter if not stored as such
         if (!inCache) return;
 
-        //Check the purpose of the interpreter, i.e. if it is a poll or if it is a bug ticket
-        if (inCache.type == "poll") {
-            //Vote on poll
-            
-            //Check whether we are removing or adding the reaction
-            if (isAdding) {
-                polls.votes.add(reaction, user);
-            }
-            else {
-                polls.votes.retract(reaction, user);
-            }
+        //Reaction interpreter format:
+        /*
+          {
+            filter: function(reactionInterpreter, isAddingReaction),
+            response: function(reaction, user)
+          }
+        */
 
-        }
-        else if (inCache.type == "poll-end") {
-            //End poll
-            
-            //If we are adding the trash reaction, end the poll
-            if (isAdding) {
-                polls.polls.endByReaction(reaction, user);
-            }
+        var intp = interpreters.reaction.find(elem => elem.filter(inCache, isAdding));
 
-        }
-        else if (inCache.type == "bug-ticket" && isAdding) {
-            //Bug ticket stuff
-
-            //Remove the users' reaction if not a bot
-            reaction.users.cache.array().forEach((u) => {
-                if (u.bot) return;
-                reaction.users.remove(u);
-            });
-
-            //Create and handle the user's bug ticket
-            report.ticket(message, user);
-        }
+        if (intp) intp.response(reaction, user);
 
     }
 
-    this.fetchReactionInterpreters = (client) => {
-        var cache = Reactions.get();
+    /**
+     * Registers interpreters of any specified type.
+     * @param {Object} options - All options for the interpreter to register.
+     * @param {String} options.type - The type of interpreter (message/dm/reaction) to register.
+     * @param {Function} options.filter - A function that accepts (args) for messages/dms or (reactionInterpreter, isAdding) for reactions; and checks whether or not the input should be responded to.
+     * @param {Function} options.response - A function that accepts (message, args) for messages/dms or (reaction, user) for reactions; and uses these parameters to respond to an interpreted input that passes the filter check.
+     */
+    this.register = ({type, filter, response}) => {
+
+      var intp = {
+        filter: filter,
+        response: response
+      };
+
+      type = type.toLowerCase();
+      if (type.endsWith("s")) type = type.slice(0, type.length - 1);
+
+      interpreters[type].push(intp);
+
+      return this;
+
+    }
+
+    /**
+     * Adds an interpretable reaction to the database.
+     */
+    this.addReaction = (emojis, obj) => {
+
+      if (!emojis) {
+        //Catch case in which no emojis provided
+      }
+      else if (Array.isArray(emojis)) {
+        //Multiple emojis
+
+        obj.name = obj.name || [];
+        obj.id = obj.id || [];
+
+        emojis.forEach(emote => {
+          if (isNaN(emote)) {
+            obj.name.push(emote);
+          }
+          else obj.id.push(emote);
+        })
+
+      }
+      else {
+        //One emoji
+
+        if (isNaN(emojis)) obj.name = obj.name || emojis;
+        else obj.id = obj.id || emojis;
+
+      }
+
+      Reactions.push(obj);
+
+      return this;
+    }
+
+    /**
+     * Initializes all Interpreters. Formerly named 'fetchReactionInterpreters()'.
+     */
+    this.initialize = (client) => {
+
+      //Initialize Reaction Interpreters
+
+        var cache = Reactions.values();
 
         cache.forEach(entry => {
             //Fetch and cache all messages that need their reactions interpreted
@@ -109,4 +157,4 @@ function Interpreter(message) {
 
 }
 
-module.exports = Interpreter;
+module.exports = new Interpreter();
