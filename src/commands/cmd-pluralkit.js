2d013aeaec430c08990a85016dc2d6c0e59ece51
Kurumi78 <30587387+Kurumi78@users.noreply.github.com>
2024-07-08 21:34:22 -0500

    Update cmd-pluralkit.js

--- src/commands/cmd-pluralkit.js
+++ src/commands/cmd-pluralkit.js
@@ -1,9 +1,9 @@
 const Vidar = require("djs-vidar");
 
-Vidar.command("version", "Returns the current version of Panacea.")
+Vidar.command("pluralkit", "Explains what Pluralkit does and who it is for.")
 .action(async i => {
     i.reply({
-        ephemeral: true,
-        content: "> ### Panacea v4.0.1"
+        ephemeral: false,
+        content: ">>> Why PluralKit? Plurality is the existence of multiple self-aware entities inside the same brain. Each member of a system/collective (group residing inside the same physical brain) is an individual person, so please treat them as such. On Discord, some systems may use PluralKit to make their message appear with the correct profile, resulting in a [BOT] tag. Note: We do NOT tolerate the use of PluralKit for role playing."
     });
 });
